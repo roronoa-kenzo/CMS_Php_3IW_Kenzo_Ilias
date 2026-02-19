816ed5522314199d2d0e0e86ecf3e381af300487
kenzo.tanougast <kenzo.tanougast@xxii.fr>
2026-02-19 13:53:36 +0100

    feat: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./www/**/*.php",
+    "./www/**/*.html",
+    "./www/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
